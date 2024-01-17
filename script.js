c57f6776bce0329e5d17446bf8a8cb058edfff2d
Matias Nicotra <matiasanicotra@gmail.com>
2024-01-17 17:28:49 -0300

    event listeners to handle mouseover events on elements

--- script.js
+++ script.js
@@ -4,7 +4,20 @@ let currentValue = 0;
 const limit = 5;
 
 const html = Array.from(Array(limit)).map((_, i) => {
-    return `<div class="item item-${i}" data-pos"${i}"></div>`;
+    return `<div class="item item-${i}" data-pos="${i}"></div>`;
 });
 
 ratingContainer.innerHTML = html.join("");      //transform to string
+
+document.querySelectorAll(".item").forEach((item) => {
+    item.addEventListener("mouseover", (e) => {
+        const pos = item.getAttribute("data-pos");
+
+        for (let i = 0; i <= pos; i++) {
+            const square = document.querySelector(`.item-${i}`);
+            if (!square.classList.contains("item-full")) {
+                square.classList.add("item-full");
+            }
+        }
+    });
+});
